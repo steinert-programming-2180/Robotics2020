c610754ed7f8b2b2629b5d286bb63f82c0c0181d
NumberC <fadi50045000@gmail.com>
2020-02-21 15:25:11 -0500

    Spark GyroPID and VelocityPID

--- AngularVelocityPID/src/main/java/frc/robot/Robot.java
+++ AngularVelocityPID/src/main/java/frc/robot/Robot.java
@@ -7,22 +7,15 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.kauailabs.navx.frc.AHRS;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpiutil.math.MathUtil;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -34,21 +27,14 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-  CANSparkMax left1, left2, left3;
-  CANSparkMax right1, right2, right3;
-  CANSparkMax[] leftMotors = new CANSparkMax[3];
-  CANSparkMax[] rightMotors = new CANSparkMax[3];
-
   AHRS ahrs;
-  ControlMode cm = ControlMode.PercentOutput;
-  double Kp = -0.335938;
-  double Ki = 0.2;
-  double Kd = 0.3;
-  double integral, previous_error, setpoint = 0;
+  TalonSRX[] leftMotors = new TalonSRX[3];
+  TalonSRX[] rightMotors = new TalonSRX[3];
+  Double Kp = 0.0;
+  Double Ki = 0.0;
+  Double Kd = 0.0;
   PIDController controller = new PIDController(Kp, Ki, Kd);
-  Joystick joy = new Joystick(0);
-  Joystick joy2 = new Joystick(1);
-  
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -58,63 +44,45 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-
     ahrs = new AHRS(SPI.Port.kMXP);
-    left1 = new CANSparkMax(1, MotorType.kBrushless);
-    left2 = new CANSparkMax(2, MotorType.kBrushless);
-    left3 = new CANSparkMax(3, MotorType.kBrushless);
-    right1 = new CANSparkMax(4, MotorType.kBrushless);
-    right2 = new CANSparkMax(5, MotorType.kBrushless);
-    right3 = new CANSparkMax(6, MotorType.kBrushless);
-
-    leftMotors[0] = left1;
-    leftMotors[1] = left2;
-    leftMotors[2] = left3;
-
-    rightMotors[0] = right1;
-    rightMotors[1] = right2;
-    rightMotors[2] = right3;
-
-    controller.setP(0.003);
-    controller.setI(0.0002);
-    controller.setD(0.01);
-  }
-
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
-
-  @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-    SmartDashboard.putNumber("P", 0);
-    SmartDashboard.putNumber("I", 0);
-    SmartDashboard.putNumber("D", 0);
-    controller.setSetpoint(ahrs.getAngle() + 90);
+    leftMotors[0] = new TalonSRX(1);
+    leftMotors[1] = new TalonSRX(2);
+    leftMotors[2] = new TalonSRX(3);
+    rightMotors[0] = new TalonSRX(4);
+    rightMotors[1] = new TalonSRX(5);
+    rightMotors[2] = new TalonSRX(6);
+    controller.setSetpoint(ahrs.getAngle()+45);
+    
   }
 
   /**
-   * This function is called periodically during operator control.
+   * This function is called every robot packet, no matter the mode. Use this for items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
    */
   @Override
-  public void teleopPeriodic() {
-    SmartDashboard.putNumber("ANGLE", ahrs.getAngle());
-    SmartDashboard.putNumber("PID", MathUtil.clamp(controller.calculate(ahrs.getAngle()), -1, 1));
-    SmartDashboard.putNumber("Error", controller.getPositionError());
-
-    for(CANSparkMax i : leftMotors){
-      i.set(MathUtil.clamp(controller.calculate(ahrs.getAngle()), -1, 1));
-    }
-
-    for(CANSparkMax i : rightMotors){
-      i.set(MathUtil.clamp(controller.calculate(ahrs.getAngle()), -1, 1));
-    }
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
   }
 
-    /**
+  /**
+   * This function is called once each time the robot enters Disabled mode.
+   */
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  /**
    * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
    */
   @Override
@@ -132,20 +100,28 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousPeriodic() {
-    SmartDashboard.putNumber("WeirdAngle", ahrs.getYaw());
+  }
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
   }
 
   /**
-   * This function is called once each time the robot enters Disabled mode.
+   * This function is called periodically during operator control.
    */
   @Override
-  public void disabledInit() {
+  public void teleopPeriodic() {
+    SmartDashboard.putNumber("X", ahrs.getVelocityX());
+    SmartDashboard.putNumber("Y", ahrs.getVelocityY());
+    SmartDashboard.putNumber("Z", ahrs.getVelocityZ());
   }
-
-  @Override
-  public void disabledPeriodic() {
-  }
-
 
   @Override
   public void testInit() {
